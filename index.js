9879bec9436bbbe1a50c4647db681d6846ea9284
MarkBrianKariuki <markbrian.karanja@student.moringaschoool.com>
2022-06-10 21:39:23 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+const record = [
+        {year:"2015", result : "W"},
+        {year : "2014", result: "N/A"},
+        {year:"2013", result: "N/A"}
+    ]
+function superbowlWin(output){
+    for (const object of output){
+        if(object.result ==="W"){
+            return object.year
+
+        }
+        
+    }
+}
+record.find(superbowlWin);
